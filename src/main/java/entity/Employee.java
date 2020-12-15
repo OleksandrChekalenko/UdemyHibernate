c53bb82ee6a72c5ff8e7721915b7ac3e351e0140
oleksandrchekalenko <whibleykevin@gmail.com>
2020-12-15 20:37:16 +0200

    Hibernate - JPA + DB + POJO

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,84 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Column(name = "id")
+    @Id
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surName")
+    private String surName;
+    @Column(name = "department")
+    private String department;
+    @Column(name = "salary")
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surName, String department, int salary) {
+        this.name = name;
+        this.surName = surName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
